17934f29d867863c7c090e161802d87b8fe85dde
orbwalk14 <coledetrick@gmail.com>
2024-12-15 20:28:23 -0600

    Requirement B1, backend language translation functionality.

---
+++ src/main/java/edu/wgu/d387_sample_code/Translations/DisplayWelcomeMessageController.java
@@ -0,0 +1,26 @@
+package edu.wgu.d387_sample_code.Translations;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import java.util.Locale;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class DisplayWelcomeMessageController {
+
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> showWelcome (@RequestParam("lang") String lang) {
+        Locale locale = Locale.forLanguageTag(lang);
+        DisplayWelcomeMessage message = new DisplayWelcomeMessage(locale);
+
+        return new ResponseEntity<String> (message.getWelcomeMessage(), HttpStatus.OK);
+    }
+
+
+}
