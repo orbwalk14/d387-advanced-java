ba0199b50c92a42137445658269a4f7f8080e785
orbwalk14 <coledetrick@gmail.com>
2024-12-15 22:40:10 -0600

    Requirement B3, Display the time for an online live presentation held at the Landon Hotel.

---
+++ src/main/java/edu/wgu/d387_sample_code/Timezone/TimeConvert.java
@@ -0,0 +1,27 @@
+package edu.wgu.d387_sample_code.Timezone;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@CrossOrigin(origins = "http://localhost:4200")
+public class TimeConvert {
+
+    public static String getTime(){
+
+        ZonedDateTime time = ZonedDateTime.now();
+
+        ZonedDateTime ET = time.withZoneSameInstant(ZoneId.of("America/New_York"));
+        ZonedDateTime MT = time.withZoneSameInstant(ZoneId.of("America/Denver"));
+        ZonedDateTime UTC = time.withZoneSameInstant(ZoneId.of("UTC"));
+
+        String timePattern = "hh:mm a";
+        DateTimeFormatter timePatternFormatter = DateTimeFormatter.ofPattern(timePattern);
+
+        String displayTimes = ET.format(timePatternFormatter) + " ET / " + MT.format(timePatternFormatter) + " MT / " + UTC.format(timePatternFormatter) + " UTC";
+
+        return displayTimes;
+    }
+}
